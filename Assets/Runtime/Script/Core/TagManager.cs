6338546ce0cb54ba8afc9956925c22375934f9af
Allan <allanfelipedk@gmail.com>
2022-05-15 21:45:32 -0300

    Implementing player game start animation

--- Assets/Runtime/Script/Core/TagManager.cs
+++ Assets/Runtime/Script/Core/TagManager.cs
@@ -6,4 +6,5 @@ public static class TagManager
 {
     public const string IsJumping = "IsJumping";
     public const string JumpMultiplier = "JumpMultiplier";
+    public const string StartGameTrigger = "StartGameTrigger";
 }
