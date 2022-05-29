359205065aad0cfeb35458265202e2486b49e9b2
Allan <allanfelipedk@gmail.com>
2022-05-29 17:47:32 -0300

    Creating components to spawn random obstacles on tracks

--- Assets/Runtime/Script/Level/EndlessTrackGenerator.cs
+++ Assets/Runtime/Script/Level/EndlessTrackGenerator.cs
@@ -5,22 +5,30 @@ using UnityEngine;
 public class EndlessTrackGenerator : MonoBehaviour
 {
     [SerializeField] private TrackSegment[] segmentsPrefabs;
+    [SerializeField] private TrackSegment  firstTrackPrefab;
+    [SerializeField] private int initialTrackCount = 10;
+
     private List<TrackSegment> currentSegments = new List<TrackSegment>();
     private void Start()
     {
-        TrackSegment initialTrack = Instantiate(segmentsPrefabs[0], transform);
+        TrackSegment initialTrack = Instantiate(firstTrackPrefab, transform);
         currentSegments.Add(initialTrack);
 
         TrackSegment previousTrack = initialTrack;
-        foreach(var trackPrefabs in segmentsPrefabs)
+        for(int i = 0; i < initialTrackCount; i++)
         {
-            TrackSegment trackInstance = Instantiate(trackPrefabs, transform);
-            trackInstance.transform.position = previousTrack.End.position
-                + (trackInstance.transform.position - trackInstance.Start.position);
+            int index = Random.Range(0, segmentsPrefabs.Length);
+            TrackSegment track = segmentsPrefabs[index];
+            TrackSegment trackIntance = Instantiate(track, transform);
+            trackIntance.transform.position = previousTrack.End.position + 
+                (trackIntance.transform.position - trackIntance.Start.position);
 
+            foreach(var obstacleSpawner in trackIntance.ObstacleSpawners)
+            {
+                obstacleSpawner.SpawnObstacle();
+            }
 
-            currentSegments.Add(trackInstance);
-            previousTrack = trackInstance;
+            previousTrack = trackIntance;
         }
     }
 }
--- Assets/Runtime/Script/Level/TrackSegment.cs
+++ Assets/Runtime/Script/Level/TrackSegment.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TrackSegment : MonoBehaviour
@@ -7,6 +5,16 @@ public class TrackSegment : MonoBehaviour
     [SerializeField] private Transform start;
     [SerializeField] private Transform end;
 
+    private ObstacleSpawner[] obstacleSpawners;
+
     public Transform Start => start;
     public Transform End => end;
+
+    public ObstacleSpawner[] ObstacleSpawners => obstacleSpawners == null ?
+        obstacleSpawners = GetComponentsInChildren<ObstacleSpawner>() : obstacleSpawners;
 }
+
+
+
+
+
