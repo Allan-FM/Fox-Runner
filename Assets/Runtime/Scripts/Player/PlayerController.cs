5d536a9d5b9fb909cdff114da621eac6a47223dc
Allan <allanfelipedk@gmail.com>
2022-06-29 15:21:11 -0300

    Move game controller to game mode

--- Assets/Runtime/Scripts/Player/PlayerController.cs
+++ Assets/Runtime/Scripts/Player/PlayerController.cs
@@ -4,7 +4,6 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private PlayerAudioController audioController;
     [SerializeField] private float horizontalSpeed = 15;
-    [SerializeField] private float forwardSpeed = 10;
 
     [SerializeField] private float laneDistanceX = 4;
 
@@ -24,15 +23,16 @@ public class PlayerController : MonoBehaviour
     Vector3 initialPosition;
 
     float targetPositionX;
+    public float ForwardSpeed { get; set; } = 10;
 
     public bool IsJumping { get; private set; }
 
     private float rollStartZ;
     public bool IsRolling { get; private set; }
 
-    public float JumpDuration => jumpDistanceZ / forwardSpeed;
+    public float JumpDuration => jumpDistanceZ / ForwardSpeed;
 
-    public float RollDuration => rollDistanceZ / forwardSpeed;
+    public float RollDuration => rollDistanceZ / ForwardSpeed;
     float jumpStartZ;
 
     private float LeftLaneX => initialPosition.x - laneDistanceX;
@@ -41,13 +41,8 @@ public class PlayerController : MonoBehaviour
     private bool CanJump => !IsJumping;
     private bool CanRoll => !IsRolling;
 
-    //TODO: Move to GameMode
-    [SerializeField] private float baseScoreMultiplier = 1;
-    private float score;
-    public int Score => Mathf.RoundToInt(score);
-
     public float TravelledDistance => transform.position.z - initialPosition.z;
-    //
+    private bool isDead = false;
 
     void Awake()
     {
@@ -57,7 +52,10 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        ProcessInput();
+        if(!isDead)
+        {
+            ProcessInput();
+        }
 
         Vector3 position = transform.position;
 
@@ -67,9 +65,6 @@ public class PlayerController : MonoBehaviour
         ProcessRoll();
 
         transform.position = position;
-
-        //TODO: Move to game mode
-        score += baseScoreMultiplier * forwardSpeed * Time.deltaTime;
     }
 
     private void ProcessInput()
@@ -101,7 +96,7 @@ public class PlayerController : MonoBehaviour
 
     private float ProcessForwardMovement()
     {
-        return transform.position.z + forwardSpeed * Time.deltaTime;
+        return transform.position.z + ForwardSpeed * Time.deltaTime;
     }
 
     private void StartJump()
@@ -170,8 +165,12 @@ public class PlayerController : MonoBehaviour
 
     public void Die()
     {
-        forwardSpeed = 0;
+        ForwardSpeed = 0;
+
+        isDead = true;
         StopRoll();
         StopJump();
+        regularCollider.enabled = false;
+        rollCollider.enabled = false;
     }
 }
